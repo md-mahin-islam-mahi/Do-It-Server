a449db16b08c3f13f5ea5ae8be85191b23266cc8
wd-mahin <wdmahin.mhm@gmail.com>
2022-12-10 09:17:04 +0600

    added post method

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
-
 require('dotenv').config();
+
 app.use(express.json());
 app.use(cors());
 
@@ -13,8 +13,24 @@ app.get('/', (req, res) => {
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try{
+        const taskListDatabase = client.db('DoItList').collection('taskList');
+
+        app.post('/addTask', (req, res) => {
+            const taskList = req.body;
+            const result = taskListDatabase.insertOne(taskList);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 app.listen(port, () => {
